14256241bd30a0af2535aff3b1af2852002faf1d
shubh0786 <43308450+shubh0786@users.noreply.github.com>
2019-03-04 13:42:53 +1300

    Add files via upload

--- index.php
+++ index.php
@@ -1,64 +1,83 @@
-<?php
+<?php Header("Cache-Control: max-age=3000, must-revalidate");
+  //connect DB
   require 'config/config.php';
   require 'config/db.php';
 
   // Create Query
   $query = 'SELECT * FROM product';
-
   // Get ibase_free_result
   $result = mysqli_query($conn, $query);
-
   //Fetch Data
   $plastics = mysqli_fetch_all($result, MYSQLI_ASSOC);
-  //var_dump($plastic);
+  //var_dump($plastics);
 
   //Free Result
   mysqli_free_result($result);
-
   //Close connection
   mysqli_close($conn);
 ?>
 
 
 
+<!-- HTML BEGINS from here -->
 <?php include('inc/header.php'); ?>
+      <div id="whatever" class="containerMain">
+        <!--<h1 id="yoo"> YO </h1>-->
+      </br></br></br>
 
-<!-- HTML BEGINS from
-<body> -->
+        <?php $myCategory = '0'; ?>                                              <!--  /* Conditional argument */ -->
+        <!-- For Loop -->
+        <?php foreach($plastics as $plastic) :
 
-      <div id="whatever">
-        <div class="container">
-
-        <h2>Products</h2>
-
-
-
-
-        <!-- Thumbnails -->
-
-
+          #define product variables
+          $category = $plastic['product_category'];
+          $productName = $plastic['product_name'];
+          $image = $plastic['image_link'];
+          $price = $plastic['price'];
+          $unit_price = $plastic['unit_price'];
+          $p_star = $plastic['plastic_rating'];
+          $id = $plastic['ID'];
 
 
-        <?php foreach($plastics as $plastic) : ?>
 
-          <div class="thumb">
-            <h6><?php echo $plastic['product_name']; ?></h6>
-                                                            <!--  /* Title */ -->
-            <img class="photoframe" src="<?php echo $plastic['image_link']; ?>" class="float-center" alt="Paris" width="304" height="236"> </br>                                           <!--  /* Image link */    -->
-            Price: <?php echo $plastic['price']; ?> Unit Price: <?php echo $plastic['unit_price']; ?></br>    <!--  /* Price */         -->
-            Plastic Rating: <?php echo $plastic['plastic_rating']; ?></br>                                    <!--  /* Crunch plastic star rating */ -->
-            <!-- <p><small>ID: <?php echo $plastic['ID']; ?></small></p> -->
-            <div product_category="center">
-            <div class="cart-action">
-              <input type="text" class="product-quantity" name="quantity" value="1" size="2" />   <!--  /* Ammount Input */ -->
-              <input type="submit" value="Add to Cart" class="btnAddAction" /></div>              <!--  /* Add to cart button */ -->
+
+          #<!-- decide product category -->
+          if($myCategory != $category) {                #if its new category, print category
+            if ($myCategory != '0') {
+              echo '</div>';
+            }
+            $myCategory = $category;
+            echo "<div class ='categoryWrapper'>";
+            echo '<h2>' . $myCategory . '</h2>';
+          }
+         #} ?>
+
+
+          <!--Print Thumbnail -->
+        <div class="cellContainer">
+      <form>
+        <div class="thumb" style="margin: 5px;">                                                            <!-- Thumb Outline -->
+          <img class="photoframe" src="<?php echo $plastic['image_link']; ?>">
+            <h4><?php echo $plastic['product_name']; ?></h4>  <br></br>                             <!--  /* Image link */    -->
+        <strong>  Price: <?php echo $plastic['price']; ?></strong> <br> Unit Price: <?php echo $plastic['unit_price']; ?></br>            <!--  /* Price */         -->
+          Plastic Rating: <?php echo $plastic['plastic_rating']; ?></br> <br></br>                                           <!--  /* Crunch plastic star rating */ -->
+          <!-- <p><small>ID: <?php echo $plastic['ID']; ?></small></p> -->                                                                   <!--  /* ID - disabled by default */ -->
+
+            <div class="cart-action">                                                                                       <!--  /* ADD TO CART functionality */ -->
+              <!-- <input type="text" class="product-quantity" name="quantity" value="1" size="2" />  --> <!-- quantity -->                  <!--  /* Ammount Input */ -->
+             <button class= "btn-success my-2 my-sm-0" type="submit">Add to Cart</button>
+            </div>
           </div>
-</div>
+        </div>
+      </form>
+
+
 
 
     <!--  /* Repeat Printing */            -->
     <?php endforeach; ?>
     </div>
-    <!-- End of thumbnails -->
-    <!-- </body -->
+    <!-- End of thumbnail display -->
+
+
     <?php include('inc/footer.php'); ?>
